0aa4b7968c87dd777e5c52ba326eadf6f24e9726
Larissa Vieira <lv23@nyu.edu>
2022-05-21 16:17:43 -0400

    Set up connection to MongoDB

---
+++ db/connection.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const mongoURI =
+  process.env.NODE_ENV === "production"
+    ? process.env.DB_URL
+    : process.env.MY_URI;
+
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((instance) =>
+    console.log(`Connected to db: ${instance.connections[0].name}`)
+  )
+  .catch((error) => console.log("Connection failed!", error));
+
+module.exports = mongoose;
