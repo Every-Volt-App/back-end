c4c396ad4a3c3db9c7cd94621b22785b73d766a2
Larissa Vieira <lv23@nyu.edu>
2022-05-21 16:07:04 -0400

    basic user model created

---
+++ models/user-model.js
@@ -0,0 +1,25 @@
+const { required } = require("nodemon/lib/config");
+const mongoose = require("../db/connection");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      // required: true,
+    },
+    password: {
+      type: String,
+      // required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = Pattern;
