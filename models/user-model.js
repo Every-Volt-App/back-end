f374e7b8eb03d6f9e152866780feccc7dd7a92b7
Larissa Vieira <lv23@nyu.edu>
2022-05-24 22:40:29 -0400

    User model updated to address authentication and authorization

--- models/user-model.js
+++ models/user-model.js
@@ -1,4 +1,5 @@
 const mongoose = require("../db/connection");
+const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema(
   {
@@ -6,18 +7,46 @@ const UserSchema = new mongoose.Schema(
       type: String,
       required: true,
       unique: true,
+      min: 3,
+      max: 20,
     },
     email: {
       type: String,
-      // required: true,
+      required: true,
+      unique: true,
     },
     password: {
       type: String,
-      // required: true,
+      required: true,
     },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      required: true,
+    },
+    cars: [{ type: mongoose.Schema.Types.ObjectId, ref: "Car" }],
   },
   { timestamps: true }
 );
+
+UserSchema.pre("save", function (next) {
+  if (!this.isModified("password")) return next();
+  bcrypt.hash(this.password, 10, (err, passwordHash) => {
+    if (err) return next(err);
+    this.password = passwordHash;
+    next();
+  });
+});
+
+UserSchema.methods.comparePassword = function (password, cb) {
+  bcrypt.compare(password, this.password, (err, isMatch) => {
+    if (err) return cb(err);
+    else {
+      if (!isMatch) return cb(null, isMatch);
+      return cb(null, this);
+    }
+  });
+};
 
 const User = mongoose.model("User", UserSchema);
 
