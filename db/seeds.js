e232169c8455626f81dc354c6d7c53a443e3eeb1
Larissa Vieira <lv23@nyu.edu>
2022-05-28 15:04:31 -0400

    User authorization and authentication complete

--- db/seeds.js
+++ db/seeds.js
@@ -1,11 +1,11 @@
 const User = require("../models/user-model");
-const seedData = require("./user-seeds.json");
-const Location = require("../models/charger-model")
-const locationData = require("./charger-seeds.json")
+const userData = require("./user-seeds.json");
+const Location = require("../models/charger-model");
+const locationData = require("./charger-seeds.json");
 
 User.deleteMany({})
   .then(() => {
-    return User.insertMany(seedData);
+    return User.insertMany(userData);
   })
   .then(console.log)
   .catch(console.error)
